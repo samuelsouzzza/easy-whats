29d113de01339c41a531faf54b470077e9600cb8
samuel <samuel@gmail.com>
2022-11-25 22:20:22 -0300

    mascara pronta

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 
 const btn = document.querySelector('.btn-iniciar');
+const txt = document.querySelector('.txt-numero');
 function iniciarConversa(){
-    const txt = document.querySelector('.txt-numero');
     const telefone = txt.value;
     window.location.href = `https://api.whatsapp.com/send?phone=55${telefone}`;
-    console.log(telefone);
 }
 btn.addEventListener('click', iniciarConversa);
+
+function mascara(){
+    let qtd = txt.value.length;
+    if(qtd === 0){
+        txt.value += '(';
+    }else if(qtd === 3){
+        txt.value += ') ';
+    }else if(qtd === 10){
+        txt.value += '-';
+    }
+}
+txt.addEventListener('keypress', mascara);
