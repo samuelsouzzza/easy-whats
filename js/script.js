aaa66ee4750b2855eef8027d9cfbc854e6cb10bd
samuel <samuel@gmail.com>
2022-11-25 22:41:40 -0300

    add regexr

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const btn = document.querySelector('.btn-iniciar');
 const txt = document.querySelector('.txt-numero');
 function iniciarConversa(){
     const telefone = txt.value;
-    window.location.href = `https://api.whatsapp.com/send?phone=55${telefone}`;
+    const telefone2 = String(telefone).replace(/[^0-9s]/g,'');/*RegExr*/ 
+    window.location.href = `https://api.whatsapp.com/send?phone=55${telefone2}`;
+    console.log(telefone2);
 }
 btn.addEventListener('click', iniciarConversa);
 
@@ -14,7 +16,7 @@ function mascara(){
     }else if(qtd === 3){
         txt.value += ') ';
     }else if(qtd === 10){
-        txt.value += '-';
+        txt.value += ' - ';
     }
 }
 txt.addEventListener('keypress', mascara);
